2383f022df8a16718a6f41aa0c58a0a0ccf2b4c8
Maximiliano121 <148399194+Maximiliano121@users.noreply.github.com>
2025-01-26 19:14:06 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://maximiliano121.github.io',
     integrations: [
         mdx(),
         sitemap(),
